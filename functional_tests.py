f1f695f70205133e62f9999e98aa8c80f2635bfd
unknown <kangas.charles@gmail.com>
2017-08-05 15:31:43 -0500

    Font page HTML now generated from a template.

--- functional_tests.py
+++ functional_tests.py
@@ -17,12 +17,12 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('ht').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.asserEqual(
+        self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
@@ -33,13 +33,14 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.Enter)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_element_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table."
         )
 
         # There is still a text box inviting her to add another item.
